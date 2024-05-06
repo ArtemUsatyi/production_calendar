c8487ba2fa92a1643b6463fd1631b71b8bd3fba1
artemowork <artem180791@mail.ru>
2024-05-06 18:24:07 +0300

    Исправление синтаксических ошибок

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,9 +10,9 @@ public class Main {
         CalendarService calendarService = new CalendarService();
 
         LocalDate date = LocalDate.of(2024, 1, 8);
-        System.out.println("Проверка день по производственному календарю - " + calendarService.checkDateCalendar(date));
+        System.out.println("Проверить день по производственному календарю - " + calendarService.checkDateCalendar(date));
 
         LocalDateTime dateTime = LocalDateTime.of(2024, 1, 12, 12, 10);
-        System.out.println("Проверка день и времни по производственному календарю - " + calendarService.checkDateAndTimeCalendar(dateTime));
+        System.out.println("Проверка дня и времни по производственному календарю - " + calendarService.checkDateAndTimeCalendar(dateTime));
     }
 }
