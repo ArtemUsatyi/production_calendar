71511d3c34b808b6f7ef145c16de1a6ea586d38b
artemowork <artem180791@mail.ru>
2024-05-06 18:20:03 +0300

    Приложение производственный календарь

---
+++ src/main/java/org/example/service/NameTypeDaysCalendar.java
@@ -0,0 +1,12 @@
+package org.example.service;
+
+public enum NameDaysCalendar {
+    WEEKEND,
+    SATURDAY,
+    SUNDAY,
+    SHORTDAY;
+
+    public static boolean getTemplateByNameDate(String nameDate){
+        for (NameDaysCalendar nameTypeDate : )
+    }
+}
---
+++ src/main/java/org/example/service/WeekendDays.java
@@ -0,0 +1,8 @@
+package org.example.service;
+
+import java.time.LocalDate;
+import java.util.Map;
+
+public class HolidayDays {
+    Map<LocalDate, String> 
+}
