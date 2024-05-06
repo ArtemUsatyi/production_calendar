4c6f0f0763e31f9fc822878585df0ae115147ec3
artemowork <artem180791@mail.ru>
2024-05-06 21:25:14 +0300

    Исправление вывода

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,9 +10,9 @@ public class Main {
         CalendarService calendarService = new CalendarService();
 
         LocalDate date = LocalDate.of(2024, 1, 8);
-        System.out.println("Проверить день по производственному календарю - " + calendarService.checkDateCalendar(date));
+        System.out.println("Проверить день по производственному календарю (рабочий/выходной)- " + calendarService.checkDateCalendar(date));
 
         LocalDateTime dateTime = LocalDateTime.of(2024, 1, 12, 12, 10);
-        System.out.println("Проверка дня и времни по производственному календарю - " + calendarService.checkDateAndTimeCalendar(dateTime));
+        System.out.println("Проверка дня и времени по производственному календарю (рабочий/выходной)- " + calendarService.checkDateAndTimeCalendar(dateTime));
     }
 }
